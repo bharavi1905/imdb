a06027ce8670f876671a5b7bb28de1f6acc05d4d
bharavi1905 <63365950+bharavi1905@users.noreply.github.com>
2021-06-18 12:20:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ import flask
 # libraries for making count matrix and similarity matrix
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import pickle, json
+import json, requests
 
 class ShowSimilarity(object):
 	def __init__(self):
 		self.__load_data()
 
 	def __load_data(self):
-		self.__show_data = pickle.load(open('shows_data.pickle', 'rb'))
+		#self.__show_data = pickle.load(open('shows_data_filtered.pickle', 'rb'))
+		self.__show_data = pd.read_csv('shows_compressed.csv')
 		print('Loaded book data.')
 		cv = CountVectorizer()
 		count_matrix = cv.fit_transform(self.__show_data['soup'])
-		self.__cos_sim = cosine_similarity(count_matrix)
+		self.__cos_sim = cosine_similarity(count_matrix, count_matrix)
 		self.__title_to_idx = pd.Series(self.__show_data.index, index=self.__show_data['title']) # title -> idx mapping
 		print('Loaded cosine similarity matrix.')
 
@@ -35,7 +36,8 @@ class ShowSimilarity(object):
 		# Get the indices of the top 10 books (sub the first as it's the input book).
 		indices = list(scores.iloc[:11].index)
 
-		return self.__show_data.iloc[indices]
+		return indices      #, self.__show_data.iloc[indices]
+
 
 app = flask.Flask(__name__)
 showsim = ShowSimilarity()
@@ -50,24 +52,20 @@ def autocomplete():
 	filtered_shows = list(showsim.search(search)['title'].values)
 	return flask.jsonify(matching_results=filtered_shows)
 
-@app.route('/recommend')
+@app.route('/recommend', methods=['GET'])
 def recommend():
-	# Dynamic page help:
-	# https://stackoverflow.com/questions/40963401/flask-dynamic-data-update-without-reload-page/40964086
-	
+
 	searchText = flask.request.args.get('jsdata')
-
-	output = dict()
+	results = []
 	if searchText:
-		#print(f'Search text: {searchText}')
-		results = showsim.recommend(searchText)
-		if results is not None:
-			output = results[['title', 'year', 'language', 'genre', 'rating',\
-			                  	'stars', 'creators', 'episode_season_duration', 'trailer', 'nameurls']].to_dict(orient='records')#results.title.values
-
-	# TODO: Convert a fuller version to JSON rather than just an array (title, url, etc.) and render as a table instead.
-	# https://stackoverflow.com/questions/48050769/pandas-dataframe-to-flask-template-as-a-json
-	return flask.render_template('results.html', recommendations=output)
+		indexes = showsim.recommend(searchText)
+		for idx in indexes:
+			req = requests.get('https://imdbdb.herokuapp.com/data/{}'.format(idx))
+			json_data = json.loads(req.content)
+			results.append(json_data)
+			
+	#print(results)
+	return flask.render_template('results.html', recommendations=results)
 
 if __name__ == '__main__':
     app.run()
