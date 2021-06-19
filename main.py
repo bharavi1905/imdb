861aaf6228bf736ffe8b111e1b6ccc6240767a2a
bharavi1905 <63365950+bharavi1905@users.noreply.github.com>
2021-06-19 13:09:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,69 @@
 import numpy as np
 import pandas as pd
 import flask
-# libraries for making count matrix and similarity matrix
+
+import json, requests
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import json, requests
 
-class ShowSimilarity(object):
-	def __init__(self):
-		self.load_data()
+#global data
+def create_similarity():
+    data = pd.read_csv('shows_compressed.csv')
+    print('Loaded book data')
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(data['soup'])
+    # create a similarity score matrix
+    similarity = cosine_similarity(count_matrix, count_matrix)
+    title_to_idx = pd.Series(data.index, index=data['title'])
+    return similarity, title_to_idx
 
-	def load_data(self):
-		#self.__show_data = pickle.load(open('shows_data_filtered.pickle', 'rb'))
-		self.show_data = pd.read_csv('shows_compressed.csv')
-		print('Loaded book data.')
-		cv = CountVectorizer()
-		count_matrix = cv.fit_transform(self.show_data['soup'])
-		print(type(count_matrix))
-		self.cos_sim = cosine_similarity(count_matrix, count_matrix)
-		print(type(self.cos_sim))
-		self.title_to_idx = pd.Series(self.show_data.index, index=self.show_data['title']) # title -> idx mapping
-		print('Loaded cosine similarity matrix.')
 
-	def search(self, query):
-		return self.show_data.loc[self.show_data['title'].str.contains(query, case=False)]
+def search_title(query):
+    data = pd.read_csv('shows_compressed.csv')
+    return data.loc[data['title'].str.contains(query, case=False)]
 
-	def recommend(self, title):
-		if title not in self.title_to_idx:
-			print('Title not found in index mapping.')
-			return None
 
-		# Get the index of the passed in book's title.
-		show_idx = self.title_to_idx[title]
+def recommend_index(title):
+    similarity, title_to_idx = create_similarity()
+    if title not in title_to_idx:
+        print('Title not found in index mapping')
+        return None
 
-		# Get scores from the cosine similarity matrix for this index.
-		scores = pd.Series(self.cos_sim[show_idx]).sort_values(ascending=False)
-		# Get the indices of the top 10 books (sub the first as it's the input book).
-		indices = list(scores.iloc[:11].index)
-
-		return indices      #, self.__show_data.iloc[indices]
+    show_idx = title_to_idx[title]
+    scores = pd.Series(similarity[show_idx]).sort_values(ascending=False)
+    indices = list(scores.iloc[:11].index)
+    return indices
 
 
 app = flask.Flask(__name__)
-showsim = ShowSimilarity()
 
 @app.route('/')
 def index():
-	return flask.render_template('index.html')
+    return flask.render_template('index.html')
 
 @app.route('/autocomplete', methods=['GET'])
 def autocomplete():
-	search = flask.request.args.get('q')
-	filtered_shows = list(showsim.search(search)['title'].values)
-	return flask.jsonify(matching_results=filtered_shows)
+    search = flask.request.args.get('q')
+    filtered_shows = list(search_title(search)['title'].values)
+    return flask.jsonify(matching_results=filtered_shows)
 
 @app.route('/recommend', methods=['GET'])
 def recommend():
+    searchText = flask.request.args.get('jsdata')
+    results = []
+    if searchText:
+        indexes = recommend_index(searchText)
+        for idx in indexes:
+            req = requests.get('https://imdbdb.herokuapp.com/data/{}'.format(idx))
+            json_data = json.loads(req.content)
+            results.append(json_data)
 
-	searchText = flask.request.args.get('jsdata')
-	results = []
-	if searchText:
-		indexes = showsim.recommend(searchText)
-		for idx in indexes:
-			req = requests.get('https://imdbdb.herokuapp.com/data/{}'.format(idx))
-			json_data = json.loads(req.content)
-			results.append(json_data)
-			
-	#print(results)
-	return flask.render_template('results.html', recommendations=results)
+        print(results)
+        return flask.render_template('results.html', recommendations=results)
 
 if __name__ == '__main__':
     app.run()
+
+    
+    
+    
