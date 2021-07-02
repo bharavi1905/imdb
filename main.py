9c19aebcdfb7dc5308b495c322d57a5753942c77
bharavi1905 <63365950+bharavi1905@users.noreply.github.com>
2021-07-02 10:41:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -66,78 +66,15 @@ def recommend():
 		if results is not None:
 			output = results[['title', 'year', 'language', 'genre', 'rating',\
 			                  	'stars', 'creators', 'episode_season_duration', 'description', 'trailer', 'nameurls']].to_dict(orient='records')#results.title.values
+			return flask.render_template('results.html', recommendations=output)
+		else:
+			return flask.render_template('results.html', t = 's')
 
 	# TODO: Convert a fuller version to JSON rather than just an array (title, url, etc.) and render as a table instead.
 	# https://stackoverflow.com/questions/48050769/pandas-dataframe-to-flask-template-as-a-json
 	#print(output)
-	return flask.render_template('results.html', recommendations=output)
+	
 
 
 if __name__ == '__main__':
     app.run()
-
-#global data
-# def create_similarity():
-#     data = pd.read_csv('shows_dlg.csv')
-#     data = data.loc[:6001]
-#     print('Loaded book data')
-#     cv = CountVectorizer(dtype=np.float32)
-#     count_matrix = cv.fit_transform(data['soup'])
-#     print('count')
-#     # create a similarity score matrix
-#     similarity = cosine_similarity(count_matrix).astype('float16')
-#     print('sim')
-#     title_to_idx = pd.Series(data.index, index=data['title'])
-#     print('title')
-#     return similarity, title_to_idx
-
-
-# def search_title(query):
-#     data = pd.read_csv('shows_dlg.csv')
-#     return data.loc[data['title'].str.contains(query, case=False)]
-
-
-# def recommend_index(title):
-#     similarity, title_to_idx = create_similarity()
-#     if title not in title_to_idx:
-#         print('Title not found in index mapping')
-#         return None
-
-#     show_idx = title_to_idx[title]
-#     scores = pd.Series(similarity[show_idx]).sort_values(ascending=False)
-#     indices = list(scores.iloc[:11].index)
-#     return indices
-
-
-# app = flask.Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return flask.render_template('index.html')
-
-# @app.route('/autocomplete', methods=['GET'])
-# def autocomplete():
-#     search = flask.request.args.get('q')
-#     filtered_shows = list(search_title(search)['title'].values)
-#     return flask.jsonify(matching_results=filtered_shows)
-
-# @app.route('/recommend', methods=['GET'])
-# def recommend():
-#     searchText = flask.request.args.get('jsdata')
-#     results = []
-#     if searchText:
-#         indexes = recommend_index(searchText)
-#         for idx in indexes:
-#             req = requests.get('https://imdbdb.herokuapp.com/data/{}'.format(idx))
-#             json_data = json.loads(req.content)
-#             results.append(json_data)
-
-#         print(results)
-#         return flask.render_template('results.html', recommendations=results)
-
-# if __name__ == '__main__':
-#     app.run()
-
-    
-    
-    
